7f9a603a205e3eec360fee70f0b7e5173fcc58d1
KrishnaB22 <60611114+KrishnaB22@users.noreply.github.com>
2020-12-31 11:15:13 +0530

    Add files via upload

--- score.py
+++ score.py
@@ -2,6 +2,7 @@ import json
 import numpy as np
 import os
 import joblib
+import pandas as pd
 
 
 def init():
@@ -12,10 +13,17 @@ def init():
 
 def run(data):
     try:
-        data = np.array(json.loads(data))
+        #data = np.array(json.loads(data))
+        data = json.loads(data)['data']
+        data = pd.DataFrame.from_dict(data)
         result = model.predict(data)
         # You can return any data type, as long as it is JSON serializable.
         return result.tolist()
     except Exception as e:
         error = str(e)
         return error
+
+
+#data = json.loads(data)['data']
+#data = pd.DataFrame.from_dict(data)
+#result = model.predict(data)
